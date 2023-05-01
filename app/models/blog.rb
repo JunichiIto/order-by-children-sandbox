696585caa169f45784863f808795125cc3dd7e0e
Junichi Ito <jit@sonicgarden.jp>
2023-05-01 14:08:25 +0900

    Add some patterns

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -8,18 +8,24 @@ class Blog < ApplicationRecord
   #   includes(:comments).sort_by { |blog| blog.comments.size }.reverse
   # end
 
+  # この方法だと、発行するクエリによってはPostgreSQL環境などでPG::GroupingErrorが発生する可能性がある
+  # scope :order_by_comment_count, -> do
+  #   left_joins(:comments)
+  #     .group(:id)
+  #     .order("COUNT(comments.id) DESC")
+  # end
+
   scope :order_by_comment_count, -> do
     sql = <<~SQL
       LEFT OUTER JOIN (
         SELECT c.blog_id, COUNT(*) AS cnt
-        FROM comments c 
+        FROM comments c
         GROUP BY c.blog_id
       ) comment_counts ON comment_counts.blog_id = blogs.id
     SQL
 
     joins(sql)
-      .order("comment_counts.cnt DESC")
-      .select("blogs.*, COALESCE(comment_counts.cnt, 0) AS comment_count")
+      .order(Arel.sql("COALESCE(comment_counts.cnt, 0) DESC"))
   end
 
   scope :created_today, -> { where(created_at: Date.current.all_day) }
