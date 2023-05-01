9c50fcc3dafc9477a68da0ae1a9dddc981913314
Junichi Ito <jit@sonicgarden.jp>
2023-05-01 10:24:01 +0900

    Implement order_by_comment_count

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,14 @@
 class Blog < ApplicationRecord
   has_many :comments
+
+  scope :order_by_comment_count, -> do
+    sql = <<~SQL
+      LEFT OUTER JOIN (
+        SELECT c.blog_id, COUNT(*) AS cnt
+        FROM comments c 
+        GROUP BY c.blog_id
+      ) comment_counts ON comment_counts.blog_id = blogs.id
+    SQL
+    joins(sql).order("comment_counts.cnt DESC")
+  end
 end
--- test/models/blog_test.rb
+++ test/models/blog_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class BlogTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test ".order_by_comment_count" do
+    blog_with_two_comments = Blog.create!(title: "こんにちは")
+    blog_with_two_comments.comments.create!(content: "やあ")
+    blog_with_two_comments.comments.create!(content: "やあやあ")
+
+    blog_with_one_comment = Blog.create!(title: "Hello")
+    blog_with_one_comment.comments.create!(content: "Hi")
+
+    blog_without_comments = Blog.create!(title: "Chao!")
+
+    blog_with_three_comments = Blog.create!(title: "にーはお")
+    blog_with_three_comments.comments.create!(content: "はお")
+    blog_with_three_comments.comments.create!(content: "はおはお")
+    blog_with_three_comments.comments.create!(content: "はおはおはお")
+
+    blogs = Blog.order_by_comment_count
+    assert_equal [blog_with_three_comments, blog_with_two_comments, blog_with_one_comment, blog_without_comments], blogs
+  end
 end
